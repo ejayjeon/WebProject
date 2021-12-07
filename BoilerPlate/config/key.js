8b997ad3c6440e2802ffc4c4da4e679acff29e82
jemma <ejayjeon@gmail.com>
2021-12-07 23:52:55 +0900

    1207 변경내용

--- BoilerPlate/config/key.js
+++
@@ -1,9 +0,0 @@
-// 환경변수 설정을 Deployment or Production 으로 분기처리 해주는 곳
-
-if(process.env.NODE_ENV === 'development') {
-module.exports = require('./dev');
-// 환경변수가 development라면 모듈을 dev.js 파일에서 가져온다
-} else {
-module.exports = require('./pro');
-// 그렇지 않으면 (반대) 모듈을 pro.js에서 가져온다
-}
